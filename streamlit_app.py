220a9e0b32774a3df45045c827b91a4af2c7bdc5
morispolanco <morispolanco@gmail.com>
2023-12-10 10:54:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,29 @@
 import streamlit as st
 import requests
 
-API_KEY = "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e"
-
-st.title("Leyes de Guatemala")
-
-question = st.text_input("Escribe tu pregunta:")
-
-if st.button("Responder"):
-
-    data = {
-      "apiKey": API_KEY,
-      "sessionID": "1", 
-      "history": [{"role": "user", "content": question}],
-      "powerful": True,
-      "google": True
+def get_api_response(question):
+    url = "https://api.afforai.com/api/api_completion"
+    payload = {
+        "apiKey": "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e",
+        "sessionID": "65489d7c9ad727940f2ab26f",
+        "history": [
+            {
+                "role": "user",
+                "content": question
+            }
+        ],
+        "powerful": True,
+        "google": True
     }
-    
-    response = requests.post("https://api.afforai.com/api/api_completion", json=data)
-    result = response.json()
-    
-    st.write("Respuesta:")
-    st.write(result["completion"])
+    response = requests.post(url, json=payload)
+    return response.json()
+
+def main():
+    st.title("Preguntas sobre las leyes de Guatemala")
+    question = st.text_input("Escribe tu pregunta")
+    if st.button("Enviar"):
+        response = get_api_response(question)
+        st.json(response)
+
+if __name__ == "__main__":
+    main()
